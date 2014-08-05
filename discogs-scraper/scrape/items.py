bfd160600499da653536eb5754682c99bf8a9340
boxysean <boxysean@gmail.com>
2014-08-05 01:08:47 -0400

    parsing!

--- discogs-scraper/scrape/items.py
+++ discogs-scraper/scrape/items.py
@@ -8,3 +8,17 @@ from scrapy.item import Item, Field
 class DiscogsRecord(Item):
 	title = Field()
 	label = Field()
+	catNum = Field()
+	mediaCondition = Field()
+	sleeveCondition = Field()
+	sellerNotes = Field()
+	label = Field()
+	seller = Field()
+	price = Field()
+	shipping = Field()
+
+class DiscogsSeller(Item):
+	name = Field()
+	numItems = Field()
+	country = Field()
+
--- discogs-scraper/scrape/spiders/wantlist_spider.py
+++ discogs-scraper/scrape/spiders/wantlist_spider.py
@@ -3,7 +3,9 @@ from scrapy.http import Request
 from scrape import settings
 from scrapy.http.cookies import CookieJar
 
-from scrape.items import DiscogsRecord
+from scrape.items import DiscogsRecord, DiscogsSeller
+
+import re
 
 class WantlistSpider(scrapy.Spider):
     name = 'wantlist'
@@ -32,8 +34,46 @@ class WantlistSpider(scrapy.Spider):
             scrapy.log.msg("Login failed", level=log.ERROR)
             return
 
-        for sel in response.xpath('//tbody/tr'):
-            record = DiscogsRecord()
-            record['title'] = sel.xpath('td/span[@class="br_item_title"]/text()').extract()
-            record['label'] = sel.xpath('td/span[@class="mplabel"]/text()').extract()
-            yield record
+        for idx, sel in enumerate(response.xpath('//table[contains(@class, "mpitems")]/tbody/tr')):
+            try:
+                record = DiscogsRecord()
+                descXpath = sel.xpath('td[@class="item_description"]')
+                record['title'] = descXpath.xpath('span[@class="br_item_title"]/a/text()').extract()[0].strip()
+
+                info = sel.xpath('td[@class="item_description"]/text()').extract()
+                record['catNum'] = info[6].strip()
+                record['mediaCondition'] = info[8].strip()
+                record['sleeveCondition'] = info[10].strip()
+                record['sellerNotes'] = info[11].strip()
+                record['label'] = sel.xpath('td[@class="item_description"]/a/text()').extract()[0].strip()
+
+                sellerXpath = sel.xpath('.//td[@class=" seller_info"]')
+                discogsSeller = DiscogsSeller()
+
+                sellerInfo = sellerXpath.xpath('ul/li/b/a/text()').extract()
+
+                discogsSeller['name'] = record['seller'] = sellerInfo[0].strip()
+
+                if len(sellerInfo) > 1:
+                    p = re.compile(r'.*(\d+).*')
+                    m = p.match(sellerInfo[1].strip())
+                    discogsSeller['numItems'] = int(m.group(1))
+                else:
+                    discogsSeller['numItems'] = 0
+
+                discogsSeller['country'] = filter(lambda x : len(x) != 0, [x.strip() for x in sellerXpath.xpath('ul/li/text()').extract()])[0]
+
+                priceXpath = sel.xpath('.//td[@align="center"]')
+                record['price'] = priceXpath.xpath('span[@class="price"]/text()').extract()[0]
+                record['shipping'] = priceXpath.xpath('span[@style="color:#555"]/text()').extract()[0].strip()
+
+                yield discogsSeller
+                yield record
+            except:
+                print "index %d" % idx
+                print sel
+
+                from scrapy.shell import inspect_response
+                inspect_response(response)
+
+                raise
